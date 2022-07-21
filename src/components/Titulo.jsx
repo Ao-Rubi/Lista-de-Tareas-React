a8b1700928eff12a26fb24ca3aec5ad93aaa9129
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-07-21 10:59:35 -0300

    limpiamos el projecto e instalamos bootsptrap

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <div>
+            <h1>Lista tarea</h1>
+            <hr />
+        </div>
+    );
+};
+
+export default Titulo;
